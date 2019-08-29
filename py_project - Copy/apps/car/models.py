3506257b1bccfadddba847828843548497430e33
Veselin <52138581+Ambroza888@users.noreply.github.com>
2019-08-29 15:08:48 -0700

    deleted copy
    Hey i deleted it!

--- py_project - Copy/apps/car/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-import re
-from django.utils import timezone
-import os
-from uuid import uuid4
-
-# Create your models here.
--- py_project - Copy/apps/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic.base import RedirectView
-                    
-urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^contact$', views.contact),
-    url(r'^reviews$', views.reviews),
-]
